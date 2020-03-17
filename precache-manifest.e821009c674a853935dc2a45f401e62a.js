66906d19d116a335e7ba10f29bcc1fed5c7abc81
kapkozheckshenov <k.jekshenuulu@iopent.ru>
2020-03-17 12:14:56 +0600

    Updates

---
+++ precache-manifest.e821009c674a853935dc2a45f401e62a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6c8e7237304c811ced9f6baa568fd6d",
+    "url": "/balance_refill_ts/index.html"
+  },
+  {
+    "revision": "aa2db2032023fa9ef3fb",
+    "url": "/balance_refill_ts/static/css/main.ae3b41c8.chunk.css"
+  },
+  {
+    "revision": "941672f35009a2bd1f1e",
+    "url": "/balance_refill_ts/static/js/2.73513eaa.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/balance_refill_ts/static/js/2.73513eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2db2032023fa9ef3fb",
+    "url": "/balance_refill_ts/static/js/main.9c2a1170.chunk.js"
+  },
+  {
+    "revision": "cb8d9c9502598edafa1d",
+    "url": "/balance_refill_ts/static/js/runtime-main.6d03fd48.js"
+  }
+]);
